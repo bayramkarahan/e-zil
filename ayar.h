6655654abfde15f2bdb20166e1fa754ff6bbf73f
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2020-12-26 22:32:21 +0300

    Add files via upload

--- ayar.h
+++ ayar.h
@@ -395,16 +395,30 @@ QTimeEdit *muzikBaslamaTimeEdit=new QTimeEdit;
         QMessageBox::information(this,"E-Zil 1.0",
                                  "Bu Uygulama Okullarda Zil Sisteminde Kullanılmak için Yazılmıştır."
                                  "\n"
-                                 "\n\t         Bayram KARAHAN"
-                                 "\n\tBilişim Teknolojileri Öğretmeni"
+                                 "\n*****************************************************************************"
+                                 "\n   Copyright (C) 2020 by Bayram KARAHAN                                     "
+                                 "\n   <bayramk@gmail.com>  <www.bayramkarahan.blogspot.com>                                                    "
+                                 "\n                                                                           "
+                                 "\n   This program is free software; you can redistribute it and/or modify    "
+                                 "\n   it under the terms of the GNU General Public License as published by    "
+                                 "\n   the Free Software Foundation; either version 3 of the License, or       "
+                                 "\n   (at your option) any later version.                                     "
+                                 "\n                                                                           "
+                                 "\n   This program is distributed in the hope that it will be useful,         "
+                                 "\n   but WITHOUT ANY WARRANTY; without even the implied warranty of          "
+                                 "\n   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           "
+                                 "\n   GNU General Public License for more details.                            "
+                                 "\n                                                                           "
+                                 "\n   You should have received a copy of the GNU General Public License       "
+                                 "\n   along with this program; if not, write to the                          "
+                                 "\n   Free Software Foundation, Inc.,                                         "
+                                 "\n   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .          "
+                                 "\n****************************************************************************"
                                  "\n"
-                                 "\n"
-                                 "İstek ve önerileriniz için;"
-                                 "\nE-Posta: bayramk@gmail.com"
-                                 "\nwww.bayramkarahan.blogspot.com"
                                  "\n");
     });
-        auto layout = new QGridLayout(ayarPage);
+
+    auto layout = new QGridLayout(ayarPage);
     layout->setContentsMargins(0, 0, 0,0);
     layout->setVerticalSpacing(5);
     layout->setColumnMinimumWidth(0, 37);
